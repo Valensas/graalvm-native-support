5f9a44ba3e15401ec225b547c55461eb97721cd0
Okan Okumusoglu <okan.okumusoglu@valensas.com>
2023-11-30 08:35:03 +0000

    add vault hints registrar

--- src/main/kotlin/com/valensas/nativesupport/autoconfigure/RuntimeHintsAutoConfiguration.kt
+++ src/main/kotlin/com/valensas/nativesupport/autoconfigure/RuntimeHintsAutoConfiguration.kt
@@ -4,6 +4,7 @@ import com.valensas.nativesupport.hints.CustomRuntimeHintsRegistrar
 import com.valensas.nativesupport.hints.HazelcastRuntimeHintsRegistrar
 import com.valensas.nativesupport.hints.KotlinRuntimeHintsRegistrar
 import com.valensas.nativesupport.hints.SpringDocRuntimeHintsRegistrar
+import com.valensas.nativesupport.hints.VaultRuntimeHintsRegistrar
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.ImportRuntimeHints
 
@@ -12,6 +13,7 @@ import org.springframework.context.annotation.ImportRuntimeHints
     CustomRuntimeHintsRegistrar::class,
     KotlinRuntimeHintsRegistrar::class,
     HazelcastRuntimeHintsRegistrar::class,
-    SpringDocRuntimeHintsRegistrar::class
+    SpringDocRuntimeHintsRegistrar::class,
+    VaultRuntimeHintsRegistrar::class
 )
 class RuntimeHintsAutoConfiguration
